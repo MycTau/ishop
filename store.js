80ad541fb8531b555e89d97f688be522fb047ba0
Andrey Mustaev <mustaev-08@yandex.ru>
2019-08-12 21:22:18 +0300

    Refactoring routes - childrens

--- store.js
+++ store.js
@@ -2,7 +2,7 @@ Vue.use(Vuex)// Подключение Vuex - сообщаем Vue - что ра
 
 const store_for_our_app = new Vuex.Store({
     state: {
-        counter:0,
+        // counter:0,
         goods: [
             {id: 1, name: 'Брюки мужские', qty: 35, price: 2650},
             {id: 2, name: 'Платье женское', qty: 13, price: 7500},
@@ -10,11 +10,9 @@ const store_for_our_app = new Vuex.Store({
         ]
     },
     mutations: {
-        increment: function(state) {
-            state.counter++;
-        },
-        setCounterValue: function (state,val) {
-            state.counter = val
+        updateGood: function (state, good) {
+            var index = state.goods.findIndex(el => el.id === good.id)
+            state.goods.splice(index, 1, good)
         }
     }
 })
